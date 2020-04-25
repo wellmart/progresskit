cf42ce165a205ac6717aea13de3b64c283170346
Wellington Marthas <well@dyntime.com>
2020-04-25 16:24:55 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# ProgressKit
+
+[![Build Status](https://travis-ci.org/wellmart/progresskit.svg?branch=master)](https://travis-ci.org/wellmart/progresskit)
+[![Swift 5](https://img.shields.io/badge/swift-5-blue.svg)](https://developer.apple.com/swift/)
+![Version](https://img.shields.io/badge/version-0.1.0-blue)
+[![Software License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)
+[![Swift Package Manager compatible](https://img.shields.io/badge/swift%20package%20manager-compatible-blue.svg)](https://github.com/apple/swift-package-manager)
+
+A lightweight kit to helps in use of skeleton animation.
+
+## Requirements
+
+Swift 5 and beyond.
+
+## Usage
+
+```swift
+import ProgressKit
+
+// coming soon
+```
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ Sources/UI/UIProgressBar.swift
@@ -0,0 +1,138 @@
+//
+//  ProgressKit
+//
+//  Copyright (c) 2020 Wellington Marthas
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import UIKit
+import Adrenaline
+import CodeLayout
+
+public final class UIProgressBar: UIView {
+    public static var appearance: UIProgressBarAppearance?
+    
+    @frozen
+    public enum Value {
+        case infinite
+        case percentage(value: CGFloat)
+    }
+    
+    public var value: Value = .infinite {
+        willSet {
+            updateIndicator(newValue: newValue)
+        }
+    }
+    
+    private weak var indicatorView: UIView!
+    
+    private var duration: TimeInterval {
+        return 1
+    }
+    
+    private var initialFrame: CGRect {
+        return CGRect(x: 0, y: 0, width: 0, height: bounds.height)
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    public override func didMoveToWindow() {
+        super.didMoveToWindow()
+        
+        guard window != nil, case .infinite = value else {
+            return
+        }
+        
+        DispatchQueue.main.async {
+            self.resetIndicator()
+            self.infiniteLoop()
+        }
+    }
+    
+    private func commonInit() {
+        let appearance = UIProgressBar.appearance
+        backgroundColor = appearance?.progressBarSecondaryColor
+        
+        indicatorView = addSubview(UIView(frame: initialFrame)) {
+            $0.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            $0.backgroundColor = appearance?.progressBarPrimaryColor
+        }
+        
+        resetIndicator()
+    }
+    
+    private func resetIndicator() {
+        indicatorView.layer.removeAllAnimations()
+        indicatorView.frame = initialFrame
+        indicatorView.layoutIfNeeded()
+    }
+    
+    private func updateIndicator(newValue: Value) {
+        switch newValue {
+        case let .percentage(value):
+            UIView.animate(withDuration: duration, delay: 0, options: [.beginFromCurrentState], animations: {
+                self.indicatorView.frame = CGRect(x: 0, y: 0, width: self.bounds.width * value, height: self.bounds.height)
+            })
+            
+        case .infinite:
+            if case .infinite = value {
+                return
+            }
+            
+            resetIndicator()
+            infiniteLoop()
+        }
+    }
+    
+    private func infiniteLoop() {
+        guard case .infinite = value else {
+            return
+        }
+        
+        let bounds = self.bounds
+        let halfDuration = duration / 2
+        
+        resetIndicator()
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0, animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: halfDuration, animations: {
+                self.indicatorView.frame = CGRect(x: 0, y: 0, width: bounds.width * 0.7, height: bounds.height)
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: halfDuration, relativeDuration: halfDuration, animations: {
+                self.indicatorView.frame = CGRect(x: bounds.width, y: 0, width: bounds.width * 0.3, height: bounds.height)
+            })
+        }) { completed in
+            guard completed else {
+                return
+            }
+            
+            self.infiniteLoop()
+        }
+    }
+}
